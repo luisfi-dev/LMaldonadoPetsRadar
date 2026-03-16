df568b62e4d032bca20f916d961fb4f049f313e4
LuisFerLCC <luisfern837@gmail.com>
2026-03-16 12:26:23 -0600

    Add DB container + env

---
+++ src/config/env.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import envVar from 'env-var';
+
+export const env = {
+  PORT: envVar.get('PORT').required().asPortNumber(),
+  MAPBOX_TOKEN: envVar.get('MAPBOX_TOKEN').required().asString(),
+  MAILER_EMAIL: envVar.get('MAILER_EMAIL').required().asString(),
+  MAILER_PASSWORD: envVar.get('MAILER_PASSWORD').required().asString(),
+  MAILER_SERVICE: envVar.get('MAILER_SERVICE').required().asString(),
+  DB_HOST: envVar.get('DB_HOST').required().asString(),
+  DB_PORT: envVar.get('DB_PORT').required().asPortNumber(),
+  DB_USER: envVar.get('DB_USER').required().asString(),
+  DB_PASSWORD: envVar.get('DB_PASSWORD').required().asString(),
+  DB_NAME: envVar.get('DB_NAME').required().asString(),
+};
